0395ab2c86ff2886583f61b8fd3dc2d4086a9596
vanthinh01052001 <vanthinh01052001@gmail.com>
2024-10-13 22:20:23 +0700

    init

---
+++ DTB.py
@@ -0,0 +1,13 @@
+# Nhập họ tên
+ho_ten = input("Nhập vào họ tên: ")
+
+# Nhập điểm các môn
+diem_toan = float(input("Nhập vào điểm môn Toán: "))
+diem_van = float(input("Nhập vào điểm môn Văn: "))
+diem_anh = float(input("Nhập vào điểm môn Anh: "))
+
+# Tính điểm trung bình
+diem_trung_binh = (diem_toan + diem_van + diem_anh) / 3
+
+# Xuất kết quả
+print(f"Học sinh {ho_ten} đạt điểm trung bình các môn là: {diem_trung_binh:.2f}")
